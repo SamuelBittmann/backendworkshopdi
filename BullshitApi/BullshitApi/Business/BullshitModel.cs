3d77fe2af28b6a383f47c509b8007f723a787d13
Samuel Bittmann <sbittmann@bluewin.ch>
2019-10-18 11:23:10 +0200

    Upgraded to dotnet core 3.0

--- BullshitApi/BullshitApi/Business/BullshitModel.cs
+++ BullshitApi/BullshitApi/Business/BullshitModel.cs
@@ -7,13 +7,7 @@ namespace BullshitApi.Business
 {
     public class BullshitModel
     {
-        public readonly string Bullshitter;
-        public readonly string Text;
-
-        public BullshitModel(string bullshitter, string text)
-        {
-            Bullshitter = bullshitter;
-            Text = text;
-        }
+        public string Bullshitter { get; set; }
+        public string Text { get; set; }
     }
 }
--- BullshitApi/BullshitApi/Startup.cs
+++ BullshitApi/BullshitApi/Startup.cs
@@ -25,13 +25,16 @@ namespace BullshitApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
         }
     }
 }
